de34acb08ae60504e402412448d62c1c031c337f
adrianFD22 <adrianfd22399@gmail.com>
2023-09-10 14:14:59 +0200

    Improve scoping

--- delete_banner.js
+++ delete_banner.js
@@ -8,49 +8,61 @@
 //      Remove banners
 //---------------------------
 
-var banner_patterns = ["cookie", "Cookie"];
-var container_patterns = ["popup"];
+function remove_banners() {
+    var banner_patterns = ["cookie", "Cookie"];
+    var container_patterns = ["popup"];
 
-var banner_regexps = Array.from(banner_patterns, pattern => new RegExp(pattern), "i");
-var container_regexps = Array.from(container_patterns, pattern => new RegExp(pattern), "i");
+    var banner_regexps = Array.from(banner_patterns, pattern => new RegExp(pattern), "i");
+    var container_regexps = Array.from(container_patterns, pattern => new RegExp(pattern), "i");
 
-var list_of_elements = document.querySelectorAll("body *");
+    var list_of_elements = document.querySelectorAll("body *");
 
 
-list_of_elements.forEach( element => {
+    list_of_elements.forEach( element => {
 
-    // Delete fixed and sticky elements
-    var pos = getComputedStyle(element).position;
+        // Delete fixed and sticky elements
+        var pos = getComputedStyle(element).position;
 
-    if (pos === "fixed" || pos == "sticky") {
-        element.remove();
-    }
-
-    // Remove banners by identifier
-    banner_regexps.forEach( regexps => {
-        if (regexps.test(element.id)) {
+        if (pos === "fixed" || pos == "sticky") {
             element.remove();
         }
-    });
 
-    // Remove classnames that make banner containers
-    var list_of_class_names = element.classList;
-
-    list_of_class_names.forEach( class_name => {
-        container_regexps.forEach( regexp => {
-            if (regexp.test(class_name)) {
-                list_of_class_names.remove(class_name);
+        // Remove banners by identifier
+        banner_regexps.forEach( regexps => {
+            if (regexps.test(element.id)) {
+                element.remove();
             }
         });
-    });
 
-});
+        // Remove classnames that make banner containers
+        var list_of_class_names = element.classList;
+
+        list_of_class_names.forEach( class_name => {
+            container_regexps.forEach( regexp => {
+                if (regexp.test(class_name)) {
+                    list_of_class_names.remove(class_name);
+                }
+            });
+        });
+
+    });
+}
 
 
 //---------------------------
 //       Fix scrolling
 //---------------------------
 
-document.body.style.setProperty("position", "static", "important");
-document.body.style.setProperty("overflow", "scroll", "important");
-document.body.style.setProperty("margin-top", "0px", "important");
+function fix_scrolling() {
+    document.body.style.setProperty("position", "static", "important");
+    document.body.style.setProperty("overflow", "scroll", "important");
+    document.body.style.setProperty("margin-top", "0px", "important");
+}
+
+
+
+//---------------------------
+//          Main
+//---------------------------
+remove_banners();
+fix_scrolling();
